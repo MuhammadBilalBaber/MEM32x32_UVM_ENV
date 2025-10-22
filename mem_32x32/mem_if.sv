246ea79dff28a62d39576534b27d2f02b6375489
Muhammad Bilal Baber <113287645+MuhammadBilalBaber@users.noreply.github.com>
2025-10-22 14:20:32 +0500

    Implement assertions for memory interface behavior
    Added assertions to verify memory behavior on read and write operations, ensuring correct signals for empty state and data output.

--- mem_32x32/mem_if.sv
+++ mem_32x32/mem_if.sv
@@ -7,5 +7,60 @@ interface mem_if (input logic clk, input logic rst_n);
   logic full;
   logic empty;
   logic half_full;
+
+  // Assertion to check the propert for memory empty
+// when reset is active then in the next cycle the empty signal should be high
+
+  property mem_empty;
+    @(posedge clk)
+      disable iff(rst_n)
+      !rst_n |=> empty;
+  endproperty
+
+  assert_mem_empty: assert property(mem_empty)
+    else $error("Memory is not empty");
+
+  // This assertion will check the behavior if the 
+  // memory is empty and still the user try to read something 
+  // then the data return to the user should be 0.
+
+  property read_if_mem_empty;
+     @(posedge clk)
+       disable iff(!rst_n)
+       (empty && rd_en ) |-> !data_out;
+  endproperty
+
+   assert_read_if_empty: assert property(read_if_mem_empty)
+    else $error("WHen mem is empty data_out is not zero");
+
+// This assertion will check if the user write something to memory
+// then empty should be deasserted. 
+
+  property if_wr_en;
+     @(posedge clk)
+     disable iff(!rst_n)
+     // wr_en |-> ##1 !empty;
+     wr_en |=>  !empty;
+  endproperty
+
+  assert_if_wr_en: assert property(if_wr_en)
+    else $error("After write to mem empty is still high!");
+
+// This is to check when there is a read rqeuest then the  dataout
+// should never be "x" or "z"
+  property if_rd_en;
+    @(posedge clk)
+    disable iff(!rst_n)
+    rd_en |=> !$isunknown(data_out); 
+  endproperty
+
+  assert_if_rd_en: assert property(if_rd_en)
+    else $error("The data_out is unknown after rd_en");
+
+  //   property mem_data_out;
+  //   @(posedge clk) empty -> data_out;
+  // endproperty
+
+  //   assert property(mem_data_out);
   
 endinterface : mem_if
